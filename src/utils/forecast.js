f7d056992863ad769a2902698e98c887fed94688
Venkatesh Babu <polineni36@gmail.com>
2020-05-26 11:10:14 +0530

    spelling changed from degress to degrees

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions + ' It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degrees Celsius.')
+            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees Celsius.')
         }
     })
 }
